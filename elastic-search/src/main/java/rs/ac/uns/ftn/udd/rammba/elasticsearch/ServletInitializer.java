b918c16d0c542714cab5e1749c11f452330d9917
Radoš Milićev <40705899+rammba@users.noreply.github.com>
2022-06-26 22:38:24 +0200

    init spring app

---
+++ elastic-search/src/main/java/rs/ac/uns/ftn/udd/rammba/elasticsearch/ServletInitializer.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.udd.rammba.elasticsearch;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ElasticSearchApplication.class);
+	}
+
+}
---
+++ elastic-search/src/test/java/rs/ac/uns/ftn/udd/rammba/elasticsearch/ElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.udd.rammba.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
